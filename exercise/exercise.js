d195cbec25f03f46e204eb15a40db348bf104bd1
RobiChiri <robertochiriac8@gmail.com>
2023-11-09 08:58:15 +0100

    esercizio-34

---
+++ exercise/exercise.js
@@ -0,0 +1,13 @@
+function printAsyncName(callback, name) {
+	setTimeout(() => {
+		callback();
+	}, 1000);
+
+	setTimeout(() => {
+		console.log(name);
+	}, 2000);
+}
+
+printAsyncName(() => {
+	console.log("Hello");
+}, "Roberto");
